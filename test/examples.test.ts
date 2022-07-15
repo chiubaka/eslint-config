55adb8e8941e65030268e11e911f5f39e4493747
Daniel Chiu <daniel@chiubaka.com>
2022-07-14 21:18:08 -0400

    Transfer remaining linting rules into .eslintrc.ts

--- test/examples.test.ts
+++ test/examples.test.ts
@@ -3,4 +3,5 @@ import { testEsLintExample } from "./utils";
 describe(".eslintrc", () => {
   testEsLintExample("missing-semicolon", "Insert `;`");
   testEsLintExample("console-log", "Unexpected console statement");
+  testEsLintExample("simple-import-sort", "Run autofix to sort these imports!");
 });
---
+++ test/examples/simple-import-sort/bad.ts
@@ -0,0 +1,6 @@
+import { execSync as __ } from "node:child_process";
+import { ESLint } from "eslint";
+import "node:fs";
+import { testEsLintExample as _ } from "../../utils";
+
+const _eslint = new ESLint();
